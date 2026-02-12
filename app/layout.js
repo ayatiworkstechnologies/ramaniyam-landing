db46e97f0b53a12c8256fd6a16729fc5ed3ebffe
rubankumarsankar <rubankumarsankar@users.noreply.github.com>
2026-02-12 18:38:00 +0530

    12/2/2026 update 4

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
 import dynamic from "next/dynamic";
-
 import { Catamaran } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
-const Footer = dynamic(() => import("@/components/Footer"));
 
+const Footer = dynamic(() => import("@/components/Footer"));
 import Header from "@/components/Header";
+
 const catamaran = Catamaran({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800"],
@@ -22,19 +22,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={catamaran.variable}>
+      <Script id="gtm" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T2CTB9FR');`}
+      </Script>
       <body>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17942239966"
-          strategy="afterInteractive"
-        />
-        <Script id="google-ads-tag" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-17942239966');
-          `}
-        </Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T2CTB9FR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Header />
         {children}
         <Footer />
